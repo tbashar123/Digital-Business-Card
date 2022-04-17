6d4f7d6f730f651a196afdbe7aaec440a51572c6
Tanvir Bashar <tbashar09@gmail.com>
2022-04-17 02:50:27 -0400

    first commit

---
+++ src/components/Info.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export default function Info() {
+    return (
+        <info >
+        <h1 className = "name" > Tanvir Bashar </h1> 
+        <h1 className = "name" > Software Engineer </h1> 
+        </info>
+    )
+}
---
+++ src/components/Interests.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+export default function Navbar() {
+    return <h1> navbar component </h1>
+}
